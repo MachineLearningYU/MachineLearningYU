571ae77668fc9507945e4d0e97c121a0fdee8bad
Ali <billa.svpc@gmail.com>
2023-10-26 00:38:46 -0400

    fix: navbar blocking the title when links clicked

--- mljs.js
+++ mljs.js
@@ -3,18 +3,44 @@ let darkLightModeButton = document.getElementById("darkModeSwitcher");
 
 let darkMode = localStorage.getItem("darkMode") ? localStorage.getItem("darkMode") : "false";
 
-if (darkMode === "true"){
+if (darkMode === "true") {
     document.getElementById("root").setAttribute("data-theme", "dark");
     darkLightModeButton.checked = true;
 }
 
 darkLightModeButton.addEventListener("change", function (event) {
-    if (event.target.checked){
+    if (event.target.checked) {
         localStorage.setItem("darkMode", "true");
         document.getElementById("root").setAttribute("data-theme", "dark");
 
-    }else{
+    } else {
         localStorage.setItem("darkMode", "false");
         document.getElementById("root").setAttribute("data-theme", "light");
     }
+});
+
+
+
+// for scrolling to the link but few pixels up because of the fixed navbar
+
+const getOffsetTop = function(element) {
+    if (!element) return 0;
+    return getOffsetTop(element.offsetParent) + element.offsetTop;
+};
+
+let navbarHeight = document.getElementById("navbar").offsetHeight;
+function scrollToLink(link) {
+    let linkPosition = getOffsetTop(document.getElementById(link));
+    window.scrollTo({
+        top: linkPosition - navbarHeight
+    });
+}
+
+// event listener for the navbar links
+let navbarLinks = document.querySelectorAll(".navbar-link");
+navbarLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        scrollToLink(link.getAttribute("href").slice(1));
+    });
 });
